bdde82707495adfd1517abaefbecc8350eeb0938
Chris Ellis <chrisellis2005@gmail.com>
2020-01-15 09:18:08 +0000

    initial build

---
+++ precache-manifest.178e55a5a87237e978cc49db20a0915e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2611a59624e8dff43353dc677f99b00b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "076bff5c891a15c34ac6",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "31b2918d83d2ba74b8a4",
+    "url": "/static/css/main.28f3ba4b.chunk.css"
+  },
+  {
+    "revision": "076bff5c891a15c34ac6",
+    "url": "/static/js/2.a4822ccf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a4822ccf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31b2918d83d2ba74b8a4",
+    "url": "/static/js/main.1504476b.chunk.js"
+  },
+  {
+    "revision": "8a1b3d27d632ab2b6e38",
+    "url": "/static/js/runtime-main.4378e03c.js"
+  }
+]);
